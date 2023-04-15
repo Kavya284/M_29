ccbbb3ef3caab38c8ed8bbfaa047b24222e80110
kavya284 <kavyan@sjcem.edu.in>
2023-04-16 01:20:28 +0530

    add integer

---
+++ dayone/src/org/tnsif/introduction/Second.java
@@ -0,0 +1,14 @@
+package org.tnsif.introduction;
+
+public class Second {
+
+	public static void main(String[] args) {
+		int b=20;
+		int a=0;
+		int c=a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
