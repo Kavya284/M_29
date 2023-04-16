f87c1396773bf3e52bc4208145b1481efec8dfd0
kavya284 <kavyan@sjcem.edu.in>
2023-04-16 12:25:40 +0530

    day two

---
+++ daytwo/src/org/tnsif/compilerinputdemo/CompilerInputDemo.java
@@ -0,0 +1,13 @@
+package org.tnsif.compilerinputdemo;
+
+public class CompilerInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//compile-time input
+		float salary = 67800.50f;
+		System.out.println("Salary is: "+ salary);
+
+	}
+
+}
---
+++ daytwo/src/org/tnsif/decisionmaking/CascadedIfElseProgram.java
@@ -0,0 +1,24 @@
+package org.tnsif.decisionmaking;
+import java.util.Scanner;
+public class CascadedIfElseProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the three values: ");
+		float percentage1 = sc.nextFloat();
+		float percentage2 = sc.nextFloat();
+		float percentage3 = sc.nextFloat();
+		if(percentage1 > percentage2 && percentage1 > percentage3) {
+			System.out.println("Person 1 has highest percentage");
+		}
+		else if(percentage2 > percentage1 && percentage2 > percentage3) {
+			System.out.println("Person 2 has highest percentage");
+		}
+		else {
+			System.out.println("Person 3 has highest percentage");
+		}
+
+	}
+
+}
