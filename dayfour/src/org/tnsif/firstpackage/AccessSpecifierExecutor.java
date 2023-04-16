d0a8b5a8009973db11ae58a651b5a3a30bf4097d
kavya284 <kavyan@sjcem.edu.in>
2023-04-16 12:50:07 +0530

    day four

---
+++ dayfour/src/org/tnsif/firstpackage/AccessSpecifierExecutor.java
@@ -0,0 +1,18 @@
+package org.tnsif.firstpackage;
+
+public class AccessSpecifierExecutor {
+
+	public static void main(String[] args) {
+		Base b = new Base();
+		b.methodDefault();
+		b.methodPublic();
+		b.methodProtected();
+		
+		/*Private members cen't be accessed into
+		 * another class*/
+		//b.varPrivate;
+		//b.methodPrivate();
+		
+		System.out.println(b instanceof Base);
+	}
+}
---
+++ dayfour/src/org/tnsif/firstpackage/Base.java
@@ -0,0 +1,25 @@
+package org.tnsif.firstpackage;
+
+public class Base {
+	//declaring all access specifiers with variable
+	int varDefault = 10;
+	public String varPublic = "John";
+	private int varPrivate = 1221;
+	protected float varProtected = 34900.50f;
+	
+	//declaring all access specifiers with method
+	void methodDefault() {
+		System.out.println("Default access method " + varDefault);
+	}
+	
+	public void methodPublic() {
+		System.out.println("Public access method " + varPublic);
+	}
+	
+	private void methodPrivate() {
+		System.out.println("Private access method " + varPrivate);
+	}
+	protected void methodProtected() {
+		System.out.println("Protected access method " + varProtected);
+	}
+}
