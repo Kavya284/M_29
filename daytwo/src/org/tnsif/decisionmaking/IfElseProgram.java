d48f28293b626cb040aaca7cc255f9367a135c5b
kavya284 <kavyan@sjcem.edu.in>
2023-04-16 12:44:20 +0530

    day three

---
+++ daytwo/src/org/tnsif/decisionmaking/IfElseProgram.java
@@ -0,0 +1,20 @@
+package org.tnsif.decisionmaking;
+import java.util.Scanner;
+public class IfElseProgram {
+	/* if we have two condition to decide then we will use
+	 * if-else
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the age and weight of donor respectively: ");
+		int age = sc.nextInt();
+		int weight = sc.nextInt();
+		if(age > 18 && weight > 50) {
+			System.out.println("Eligible to donate blood");
+		}
+		else {
+			System.out.println("Not eligible to donate blood");
+		}
+	}
+
+}
---
+++ daytwo/src/org/tnsif/looping/DigitProgram.java
@@ -0,0 +1,19 @@
+package org.tnsif.looping;
+import java.util.Scanner;
+
+public class DigitProgram {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int num = sc.nextInt();
+		int rev, count = 0;
+		do {
+			num = num / 10;
+			count++;
+		}while(num > 0);
+		System.out.println("No of Digits in Given Number is: " + count);
+	}
+
+}
+
