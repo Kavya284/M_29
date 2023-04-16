2df78b40b5a3ecae2d99dbd41a9491e4b78b6dad
kavya284 <kavyan@sjcem.edu.in>
2023-04-16 11:31:47 +0530

    Primitive data type

---
+++ dayone/src/org/tnsif/introduction/PrimitiveDataType.java
@@ -0,0 +1,24 @@
+package org.tnsif.introduction;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bytemax=127;
+		short shortmax= 32767;
+		long var2=6790005668L;
+		char v1='s';
+		boolean flag=true;
+		System.out.println(bytemax);
+		System.out.println(shortmax);
+		System.out.println(var2);
+		System.out.println( "character is: " +v1);
+		System.out.println("flag value is: " + flag);
+
+
+
+
+
+	}
+
+}
